c6072c3a7f0d8e60979542d67863c5d3e707f817
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2022-06-10 14:21:49 +0200

    Tests for date

---
+++ tests/test_date.R
@@ -0,0 +1,87 @@
+library(tableschema)
+source("helpers.R")
+
+
+schema <- list(
+  name = "date",
+  title = "A date field",
+  description = "A description",
+  type = "date"
+)
+res <- to_date(c("2020-01-01", "2022-12-31", "", NA), schema = schema)
+expect_equal(res, as.Date(c("2020-01-01", "2022-12-31", NA, NA)), attributes = FALSE)
+expect_attribute(res, "schema", schema)
+expect_error(
+  to_date(c("20200101", "20221231", "", NA), schema = schema)
+)
+
+schema <- list(
+  name = "date",
+  title = "A date field",
+  description = "A description",
+  type = "date",
+  format = "default"
+)
+res <- to_date(c("2020-01-01", "2022-12-31", "", NA), schema = schema)
+expect_equal(res, as.Date(c("2020-01-01", "2022-12-31", NA, NA)), attributes = FALSE)
+expect_attribute(res, "schema", schema)
+expect_error(
+  to_date(c("20200101", "20221231", "", NA), schema = schema)
+)
+
+schema <- list(
+  name = "date",
+  title = "A date field",
+  description = "A description",
+  type = "date",
+  format = "any"
+)
+res <- to_date(c("2020-01-01", "2022-12-31", "", NA), schema = schema)
+expect_equal(res, as.Date(c("2020-01-01", "2022-12-31", NA, NA)), attributes = FALSE)
+expect_attribute(res, "schema", schema)
+expect_error(
+  to_date(c("20200101", "20221231", "", NA), schema = schema)
+)
+
+schema <- list(
+  name = "date",
+  title = "A date field",
+  description = "A description",
+  type = "date",
+  format = "%Y%m%d"
+)
+res <- to_date(c("20200101", "20221231", "", NA), schema = schema)
+expect_equal(res, as.Date(c("2020-01-01", "2022-12-31", NA, NA)), attributes = FALSE)
+expect_attribute(res, "schema", schema)
+expect_error(
+  to_date(c("2020-01-01", "2022-12-31", "", NA), schema = schema)
+)
+
+
+# === No Schema
+schema <- list(
+  type = "date"
+)
+res <- to_date(c("2020-01-01", "2022-12-31", "", NA))
+expect_equal(res, as.Date(c("2020-01-01", "2022-12-31", NA, NA)), attributes = FALSE)
+expect_attribute(res, "schema", schema)
+expect_error(
+  to_date(c("20200101", "20221231", "", NA), schema = schema)
+)
+
+# =============================================================================
+# csv_colclass
+res <- csv_colclass_date(list()) 
+expect_equal(res, "character")
+
+# =============================================================================
+# csv_format
+res <- csv_format_date(as.Date(c("2020-01-01", "2022-12-21", NA, NA)))
+expect_equal(res, c("2020-01-01", "2022-12-21", NA, NA), attributes = FALSE)
+
+schema <- list(
+  type = "date",
+  format = "%Y%m%d"
+)
+res <- csv_format_date(as.Date(c("2020-01-01", "2022-12-21", NA, NA)), schema = schema)
+expect_equal(res, c("20200101", "20221221", NA, NA), attributes = FALSE)
