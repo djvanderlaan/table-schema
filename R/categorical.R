7d3af7f8874b459571d7b9ff68c8d1e1a57f16d0
djvanderlaan <jan@ntteloos.nl>
2022-07-23 23:09:18 +0200

    Cleanup for categorical

--- R/categorical.R
+++ R/categorical.R
@@ -1,39 +1,38 @@
 
-#' Convert field to factor based on schema
-#' 
-#' @param x vector with field to convert
-#' @param schema the schema of the field
-#'
-#' @details
-#' The information on levels is contained in the 'categories' field of the
-#' field schema. This should be a vector with objects with the fields
-#' 'name' and 'title'. 'name' is the value of the field in the vector and 
-#' 'title' is name name of the level. 
-#'
-#' @examples
-#' schema <- list(
-#'   name = "field1",
-#'   type = "string",
-#'   categories = list(
-#'     list(name = "M", title= "Male"),
-#'     list(name = "F", title= "Female")
-#'   )
-#' )
-#' to_factor(c("F", "M", "F"), schema)
-#'
-#' schema <- list(
-#'   name = "field2",
-#'   type = "integer",
-#'   categories = list(
-#'     list(name = 0, title= "North"),
-#'     list(name = 1, title= "South"),
-#'     list(name = 2, title= "East"),
-#'     list(name = 3, title= "West")
-#'   )
-#' )
-#' to_factor(1:3, schema)
-#'
-#' @export
+# Convert field to factor based on schema
+# 
+# @param x vector with field to convert
+# @param schema the schema of the field
+#
+# @details
+# The information on levels is contained in the 'categories' field of the
+# field schema. This should be a vector with objects with the fields
+# 'name' and 'title'. 'name' is the value of the field in the vector and 
+# 'title' is name name of the level. 
+#
+# @examples
+# schema <- list(
+#   name = "field1",
+#   type = "string",
+#   categories = list(
+#     list(name = "M", title= "Male"),
+#     list(name = "F", title= "Female")
+#   )
+# )
+# to_factor(c("F", "M", "F"), schema)
+#
+# schema <- list(
+#   name = "field2",
+#   type = "integer",
+#   categories = list(
+#     list(name = 0, title= "North"),
+#     list(name = 1, title= "South"),
+#     list(name = 2, title= "East"),
+#     list(name = 3, title= "West")
+#   )
+# )
+# to_factor(1:3, schema)
+#
 to_factor <- function(x, schema) {
   if (is.null(schema$categories)) {
     warning("Schema does not have categories. Returning original vector.")
@@ -54,7 +53,6 @@ to_factor <- function(x, schema) {
 }
 
 
-#' @export
 csv_format_categorical <- function(x, schema = attr(x, "schema")) {
   if (is.null(schema)) schema <- build_schema(x)
   if (is.null(schema$categories)) stop("the categories element is missing ", 
